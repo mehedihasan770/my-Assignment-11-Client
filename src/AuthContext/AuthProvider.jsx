cf55a1357ba00d0cfaa20574f068b268f8145908
mehedihasan770 <mehedihasantrbd@gmail.com>
2025-12-06 12:09:11 +0600

    Add Firebase email/password signup function with loading state using createUserWithEmailAndPassword

--- src/AuthContext/AuthProvider.jsx
+++ src/AuthContext/AuthProvider.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AuthContext } from './AuthContext';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../FirebaseConfig/Firebase.Config';
 
 const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    const signUnWithEP = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
     const userInfo = {
         name: 'mehedi'
     }
+
     return <AuthContext value={userInfo}>{children}</AuthContext>
 };
 
