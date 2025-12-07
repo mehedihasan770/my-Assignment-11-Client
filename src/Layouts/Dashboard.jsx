b496453bf0479295f736beb223a71d77a2e8395f
mehedihasan770 <mehedihasantrbd@gmail.com>
2025-12-07 14:56:10 +0600

    Implement fully responsive dashboard drawer sidebar with icon-based collapsed view, tooltips on collapse, single-line ellipsis menu labels, improved navigation layout, and adaptive dark mode styling enhancements

--- src/Layouts/Dashboard.jsx
+++ src/Layouts/Dashboard.jsx
@@ -1,46 +1,97 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from '../Components/Navbar/Navbar';
+import { BiHome } from 'react-icons/bi';
+import { GrUserManager } from "react-icons/gr";
+import { MdManageSearch, MdOutlineBookmarkAdded } from "react-icons/md";
+import { GiPodiumWinner } from "react-icons/gi";
+import { ImProfile } from 'react-icons/im';
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { MdOutlineAddTask } from "react-icons/md";
+import { BsSendArrowDownFill } from "react-icons/bs";
+import { FaEdit } from 'react-icons/fa';
 
 const Dashboard = () => {
     return (
         <div className='max-w-10/12 mx-auto'>
-            <main>
-                <Outlet/>
+          <header><Navbar></Navbar></header>
+            <main className='bg-secondary mt-5 mb-5 rounded-2xl dark:bg-[#261B25]'>
                 <div className="drawer lg:drawer-open">
-  <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
-  <div className="drawer-content">
-    {/* Navbar */}
-    <nav className="navbar w-full bg-base-300">
-      <label htmlFor="my-drawer-4" aria-label="open sidebar" className="btn btn-square btn-ghost">
-        {/* Sidebar toggle icon */}
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
-      </label>
-      <div className="px-4">Navbar Title</div>
-    </nav>
-    {/* Page content here */}
-    <div className="p-4">Page Content</div>
-  </div>
+                  <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+                  <div className="drawer-content">
+                    <label htmlFor="my-drawer-4" aria-label="open sidebar" className="lg:hidden block ml-4">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 cursor-pointer inline-block size-6"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
+                    </label>
+                  <div className="p-5">
+                    <Outlet/>
+                  </div>
+                </div>
 
   <div className="drawer-side is-drawer-close:overflow-visible">
     <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
-    <div className="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-64">
-      {/* Sidebar content here */}
+    <div className="flex min-h-full flex-col bg-secondary dark:bg-[#1f1f1f] lg:rounded-l-2xl border-r-2 border-white items-start is-drawer-close:w-14 is-drawer-open:w-64">
       <ul className="menu w-full grow">
-        {/* List item */}
+        <label htmlFor="my-drawer-4" aria-label="open sidebar" className="btn lg:block hidden btn-square btn-ghost">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
+        </label>
         <li>
           <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Homepage">
-            {/* Home icon */}
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path></svg>
-            <span className="is-drawer-close:hidden">Homepage</span>
+            <BiHome className="my-1.5 inline-block size-4" />
+            <span className="is-drawer-close:hidden whitespace-nowrap overflow-hidden text-ellipsis">Homepage</span>
           </button>
         </li>
-
-        {/* List item */}
         <li>
-          <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Settings">
-            {/* Settings icon */}
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M20 7h-9"></path><path d="M14 17H5"></path><circle cx="17" cy="17" r="3"></circle><circle cx="7" cy="7" r="3"></circle></svg>
-            <span className="is-drawer-close:hidden">Settings</span>
+          <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="My Participated Contests">
+            <MdOutlineBookmarkAdded className="my-1.5 inline-block size-4" />
+            <span className="is-drawer-close:hidden whitespace-nowrap overflow-hidden text-ellipsis">My Participated Contests</span>
+          </button>
+        </li>
+        <li>
+          <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="My Winning Contests">
+            <GiPodiumWinner className="my-1.5 inline-block size-4" />
+            <span className="is-drawer-close:hidden whitespace-nowrap overflow-hidden text-ellipsis">My Winning Contests</span>
+          </button>
+        </li>
+        <li>
+          <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Create Contest">
+            <IoMdAddCircleOutline className="my-1.5 inline-block size-4" />
+            <span className="is-drawer-close:hidden whitespace-nowrap overflow-hidden text-ellipsis">Create Contest</span>
+          </button>
+        </li>
+        <li>
+          <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="My Created Contests">
+            <MdOutlineAddTask className="my-1.5 inline-block size-4" />
+            <span className="is-drawer-close:hidden whitespace-nowrap overflow-hidden text-ellipsis">My Created Contests</span>
+          </button>
+        </li>
+        <li>
+          <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Edit contest">
+            <FaEdit className="my-1.5 inline-block size-4" />
+            <span className="is-drawer-close:hidden whitespace-nowrap overflow-hidden text-ellipsis">Edit contest</span>
+          </button>
+        </li>
+        <li>
+          <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Submitted Tasks">
+            <BsSendArrowDownFill className="my-1.5 inline-block size-4" />
+            <span className="is-drawer-close:hidden whitespace-nowrap overflow-hidden text-ellipsis">Submitted Tasks</span>
+          </button>
+        </li>
+        <li>
+          <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Manage Users">
+            <GrUserManager className="my-1.5 inline-block size-4" />
+            <span className="is-drawer-close:hidden whitespace-nowrap overflow-hidden text-ellipsis">Manage Users</span>
+          </button>
+        </li>
+        <li>
+          <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Manage Contests">
+            <MdManageSearch className="my-1.5 inline-block size-4" />
+            <span className="is-drawer-close:hidden whitespace-nowrap overflow-hidden text-ellipsis">Manage Contests</span>
+          </button>
+        </li>
+        <li>
+          <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Profile">
+            <ImProfile className="my-1.5 inline-block size-4" />
+            <span className="is-drawer-close:hidden whitespace-nowrap overflow-hidden text-ellipsis">Profile</span>
           </button>
         </li>
       </ul>
